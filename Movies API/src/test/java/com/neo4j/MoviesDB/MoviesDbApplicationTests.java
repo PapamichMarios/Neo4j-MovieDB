48c5ac49e37e25816f75ef8aa3be0270bb2d9b9d
Marios Papamichalopoulos <papamichalopoulos.m@gmail.com>
2020-05-14 12:34:38 +0300

    spring neo4j api init

---
+++ Movies API/src/test/java/com/neo4j/MoviesDB/MoviesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neo4j.MoviesDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
